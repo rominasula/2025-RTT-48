1b0039e3ed21173d6277388b36818e8a5553436f
Nikhil <nikhil@citymerchandise.com>
2025-08-27 12:11:47 -0400

    Javascript Exercise

---
+++ 308_JS_Practice/script.js
@@ -0,0 +1,48 @@
+// console.log("Hello World!")
+// console.log("Good Bye")
+
+//exercise 1
+let num1 = 10;
+let num2 = 20;
+
+let sum = num1 + num2;
+console.log("Sum:", sum);
+
+//exercise2
+let a = 15;
+let b = 5;
+let c = 2;
+
+let result1 = a + b + c;   // sum
+let result2 = a * b;       // multiplication
+let result3 = a - c;       // subtraction
+
+console.log("Results:", result1, result2, result3);
+
+//exercise3
+// Declare 10 variables
+let v1 = 4, v2 = 8, v3 = 15, v4 = 23, v5 = 42;
+let v6 = 10, v7 = 20, v8 = 30, v9 = 40, v10 = 50;
+
+// Average of first 5
+let avg1 = (v1 + v2 + v3 + v4 + v5) / 5;
+
+// Average of last 5
+let avg2 = (v6 + v7 + v8 + v9 + v10) / 5;
+
+// Average of both results
+let avgFinal = (avg1 + avg2) / 2;
+
+console.log("Avg of first 5:", avg1);
+console.log("Avg of last 5:", avg2);
+console.log("Final Avg:", avgFinal);
+
+//exercise4
+
+let group1 = (4 + 10 + 22 + -30 + 55) / 5;
+let group2 = (68 + 7 + -22 + 9 + 100) / 5;
+let finalAvg = (group1 + group2) / 2;
+
+console.log("Average 1:", group1);
+console.log("Average 2:", group2);
+console.log("Final Average:", finalAvg);
