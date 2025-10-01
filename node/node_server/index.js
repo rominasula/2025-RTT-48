3da1b63393cef49a01303fc37726566ac98543a9
Nikhil <nikhil@citymerchandise.com>
2025-10-01 13:54:56 -0400

    node server

--- node/node_server/index.js
+++ node/node_server/index.js
@@ -1,11 +1,13 @@
-const http = require("http");
+// const http = require("http");
 
-http
-  .createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.write("Hello World");
-    res.end();
-  }).listen(3000, () => {
-    console.log(`Server running.`);
-  });
+// http
+//   .createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "text/html");
+//     res.write("Hello World");
+//     res.end();
+//   }).listen(3000, () => {
+//     console.log(`Server running.`);
+//   });
+
+console.log("Hello World!");
